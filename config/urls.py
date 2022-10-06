3fc3c4a1d98d3eb30a912bde3740371082ba789d
Son Chau <soncqb@gmail.com>
2022-10-06 16:43:47 -0700

    Registration completed

--- config/urls.py
+++ config/urls.py
@@ -13,17 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from blango_auth.forms import BlangoRegistrationForm
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 import debug_toolbar
 import blog.views
-
+import blango_auth.views
+from django_registration.backends.activation.views import RegistrationView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", blango_auth.views.profile,
+         name="profile"),
+    path(
+        "accounts/register/",
+        RegistrationView.as_view(form_class=BlangoRegistrationForm),
+        name="django_registration_register",
+    ),
+    path("accounts/",
+         include("django_registration.backends.activation.urls"))
 ]
 if settings.DEBUG:
     urlpatterns += [
