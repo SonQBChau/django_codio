69da1e87869a1ba1a05ba6e6ad32bed516175955
Son Chau <soncqb@gmail.com>
2022-11-09 21:15:03 -0800

    Finish Serializers

---
+++ blog/api/serializers.py
@@ -0,0 +1,69 @@
+from rest_framework import serializers
+from blog.models import Post
+from blango_auth.models import User
+
+from django.utils.text import slugify
+
+
+def is_capitalized(value):
+    if value[0].lower() == value[0]:
+        raise serializers.ValidationError("Value must be capitalized")
+
+
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    email = serializers.EmailField(required=False)
+    first_name = serializers.CharField(
+        max_length=20, required=False, validators=[is_capitalized]
+    )
+    last_name = serializers.CharField(
+        max_length=20, required=False, validators=[is_capitalized]
+    )
+    password = serializers.CharField(write_only=True, required=False)
+    join_date = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return User(**validated_data)
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        return instance
+
+    def validate_email(self, value):
+        value = value.lower()
+        domain = value.split("@")[
+            1
+        ]  # safe to do since we know value is valid email address
+        if domain != "example.com":
+            raise serializers.ValidationError("domain must be example.com")
+        return value
+
+    def validate(self, data):
+        if (not data.get("first_name")) != (not data.get("last_name")):
+            raise serializers.ValidationError(
+                "first_name and last_name must be provided together"
+            )
+        return data
+
+
+class PostSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(required=False)
+    autogenerate_slug = serializers.BooleanField(
+        required=False, write_only=True, default=False
+    )
+
+    class Meta:
+        model = Post
+        exclude = ["modified_at", "created_at"]
+
+    def validate(self, data):
+        if not data.get("slug"):
+            if data["autogenerate_slug"]:
+                data["slug"] = slugify(data["title"])
+            else:
+                raise serializers.ValidationError(
+                    "slug is required if autogenerate_slug is not set"
+                )
+        del data["autogenerate_slug"]
+        return data
