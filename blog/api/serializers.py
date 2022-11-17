763fab7f0cd62a92dc4aad6e09e2169a5361157d
Son Chau <soncqb@gmail.com>
2022-11-17 15:49:58 -0800

    Finish nested relationships

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,79 +1,32 @@
 from rest_framework import serializers
-from blog.models import Post, Tag
+from blog.models import Post, Tag, Comment
 from blango_auth.models import User
 
 from django.utils.text import slugify
-
-
-def is_capitalized(value):
-    if value[0].lower() == value[0]:
-        raise serializers.ValidationError("Value must be capitalized")
-
-
-# class UserSerializer(serializers.Serializer):
-#     username = serializers.CharField()
-#     email = serializers.EmailField(required=False)
-#     first_name = serializers.CharField(
-#         max_length=20, required=False, validators=[is_capitalized]
-#     )
-#     last_name = serializers.CharField(
-#         max_length=20, required=False, validators=[is_capitalized]
-#     )
-#     password = serializers.CharField(write_only=True, required=False)
-#     join_date = serializers.DateTimeField(read_only=True)
-
-#     def create(self, validated_data):
-#         return User(**validated_data)
-
-#     def update(self, instance, validated_data):
-#         for key, value in validated_data.items():
-#             setattr(instance, key, value)
-#         return instance
-
-#     def validate_email(self, value):
-#         value = value.lower()
-#         domain = value.split("@")[
-#             1
-#         ]  # safe to do since we know value is valid email address
-#         if domain != "example.com":
-#             raise serializers.ValidationError("domain must be example.com")
-#         return value
-
-#     def validate(self, data):
-#         if (not data.get("first_name")) != (not data.get("last_name")):
-#             raise serializers.ValidationError(
-#                 "first_name and last_name must be provided together"
-#             )
-#         return data
-
-
-# class PostSerializer(serializers.ModelSerializer):
-#     slug = serializers.SlugField(required=False)
-#     autogenerate_slug = serializers.BooleanField(
-#         required=False, write_only=True, default=False
-#     )
-
-#     class Meta:
-#         model = Post
-#         exclude = ["modified_at", "created_at"]
-
-#     def validate(self, data):
-#         if not data.get("slug"):
-#             if data.get("autogenerate_slug"):
-#                 data["slug"] = slugify(data["title"])
-#                 del data["autogenerate_slug"]
-#             else:
-#                 raise serializers.ValidationError(
-#                     "slug is required if autogenerate_slug is not set"
-#                 )
-
-#         return data
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email"]
+
+
+class TagField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_or_create(value=data.lower())[0]
+        except (TypeError, ValueError):
+            self.fail(f"Tag value {data} is invalid")
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    creator = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "creator", "content", "modified_at", "created_at"]
+        readonly = ["modified_at", "created_at"]
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -88,3 +41,20 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = "__all__"
         readonly = ["modified_at", "created_at"]
+
+
+class PostDetailSerializer(PostSerializer):
+    comments = CommentSerializer(many=True)
+
+    def update(self, instance, validated_data):
+        comments = validated_data.pop("comments")
+        instance = super(PostDetailSerializer, self).update(instance, validated_data)
+        for comment_data in comments:
+            if comment_data.get("id"):
+                # comment has an ID so was pre-existing
+                continue
+            comment = Comment(**comment_data)
+            comment.creator = self.context["request"].user
+            comment.content_object = instance
+            comment.save()
+        return instance
