470132ca81ac99b0d7ed251f3e7e039d90a5a860
Son Chau <soncqb@gmail.com>
2022-12-09 12:01:37 -0800

    Finish JavaScript promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,18 +1,20 @@
-class DelayedGreeter extends Greeter {
-  delay = 2000;
-  constructor(name, delay) {
-    super(name);
-    if (delay !== undefined) {
-      this.delay = delay;
-    }
-  }
-  greet() {
-    setTimeout(() => {
-      this.showGreeting(this.getGreeting());
-    }, this.delay);
-  }
+function resolvedCallback(data) {
+  console.log("Resolved with data " + data);
 }
-const dg2 = new DelayedGreeter("Patchy 2 Seconds");
-dg2.greet();
-const dg1 = new DelayedGreeter("Patchy 1 Second", 1000);
-dg1.greet();
+function rejectedCallback(message) {
+  console.log("Rejected with message " + message);
+}
+const lazyAdd = function (a, b) {
+  const doAdd = (resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("a and b must both be numbers");
+    } else {
+      const sum = a + b;
+      resolve(sum);
+    }
+  };
+  return new Promise(doAdd);
+};
+const p = lazyAdd(3, 4);
+p.then(resolvedCallback, rejectedCallback);
+lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback);
